729f25309d5da5ec1178b909072649ffa7cea540
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-02-11 13:11:01 -0600

    Comenzando a crear Componente Home Inicio del componente

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// componetes clases
+class Home extends React.Component{
+    render(){
+        return(
+            <p>Home</p>
+        );
+    }
+}
+
+export default Home;
